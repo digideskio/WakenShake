17029963298964525a377801afcf34ab1eb84712
Nick Ladd <laddng12@wfu.edu>
2016-01-17 22:49:11 -0500

    updated the donations seed data script to create demo Stripe charges that show how the donations look on team and dancer profiles

--- db/seeds/development/donations.seeds.rb
+++ db/seeds/development/donations.seeds.rb
@@ -4,22 +4,23 @@ after "development:dancers" do
 
   # names
   first_name_array = %w(Nick Joe John Mark Mike Jake Jessie Melissa Ann Caroline Megan Sarah)
-  last_name_array = %w(Johnson Smith Root Beshara Thompson Reed Jackson Allen Richards)
 
   Dancer.all.each do |dancer|
     rand(7..10).times do
-      dancer.donations.where(
+      dancer.charges.where(
         amount: rand(20..100),
-        name: "#{first_name_array.sample} #{last_name_array.sample}"
+        email: "#{first_name_array.sample}@gmail.com",
+        is_donation: true
       ).first_or_create!
     end
   end
 
   Team.all.each do |team|
     rand(10..15).times do
-      team.donations.where(
+      team.charges.where(
         amount: rand(20..100),
-        name: "#{first_name_array.sample} #{last_name_array.sample}"
+        email: "#{first_name_array.sample}@gmail.com",
+        is_donation: true
       ).first_or_create!
     end
   end
