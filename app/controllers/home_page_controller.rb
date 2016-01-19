3965d24d7bc19f29c5d9121bdcf3bc534b122d7c
Nick Ladd <laddng12@wfu.edu>
2016-01-19 16:06:44 -0500

    update to charges

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,7 +1,7 @@
 class HomePageController < ApplicationController
   def index
-    @top_10_teams = Team.first(10)
-    @top_10_dancers = Dancer.first(10)
+    @top_10_teams = Team.joins(:charges).where('charges.is_donation=true').select('teams.id, name, sum(charges.amount) as total_raised').group('teams.id').order('total_raised desc').limit(10)
+    @top_10_dancers = Dancer.joins(:charges).where('charges.is_donation=true').select('dancers.id, first_name, last_name, sum(charges.amount) as total_raised').group('dancers.id').order('total_raised desc').limit(10)
     @dancing_for_list = Dancer.all.where("dancing_for != ''").order("RANDOM()").limit(10)
   end
 end
