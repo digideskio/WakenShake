6fb9b23fc4c84a121d8f061b4fb0ad789406b052
Nick Ladd <laddng12@wfu.edu>
2016-01-29 22:22:48 -0500

    new db migration for events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123041752) do
+ActiveRecord::Schema.define(version: 20160130032146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,15 @@ ActiveRecord::Schema.define(version: 20160123041752) do
 
   create_table "dorms", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.string   "place"
+    t.datetime "date_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
