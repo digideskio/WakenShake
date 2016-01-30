caa24d984ee9ecd57df63231a47f8c725bc955bf
Nick Ladd <laddng12@wfu.edu>
2016-01-29 23:03:04 -0500

    events view for homepage

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -3,5 +3,6 @@ class HomePageController < ApplicationController
     @top_10_teams = Team.joins(:charges).where('charges.is_donation=true').select('teams.id, name, sum(charges.amount) as total_raised').group('teams.id').order('total_raised desc').limit(10)
     @top_10_dancers = Dancer.joins(:charges).where('charges.is_donation=true').select('dancers.id, first_name, last_name, sum(charges.amount) as total_raised').group('dancers.id').order('total_raised desc').limit(10)
     @dancing_for_list = Dancer.all.where("dancing_for != ''").order("RANDOM()").limit(10)
+    @events = Event.where("date_time > ?", Time.now.end_of_day)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  text       :text
+#  place      :string
+#  date_time  :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Event < ActiveRecord::Base
 end
