c7ac6e2e106d4d3c66e9eda22fd6d274d888170c
Nick Ladd <laddng12@wfu.edu>
2016-02-04 17:35:58 -0500

    Added a new sponsors model, controller, and views to show sponsors of Wake 'N Shake

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sponsors
   resources :champions
   resources :events
   get 'committee/index'
---
+++ test/models/sponsor_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: sponsors
+#
+#  id                :integer          not null, primary key
+#  name              :string
+#  link              :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  logo_file_name    :string
+#  logo_content_type :string
+#  logo_file_size    :integer
+#  logo_updated_at   :datetime
+#
+
+require 'test_helper'
+
+class SponsorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
