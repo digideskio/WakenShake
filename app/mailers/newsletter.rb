020c63e5a2aabe4c9ada2d4036983a752e5e0b64
Nick Ladd <laddng12@wfu.edu>
2016-02-22 18:51:40 -0500

    updates to index

--- app/mailers/newsletter.rb
+++ app/mailers/newsletter.rb
@@ -4,12 +4,12 @@ class Newsletter < ApplicationMailer
   def weekly_mailer
     @top_dancers = Dancer.joins(:charges).group('dancers.id').order('sum(charges.amount) desc').limit(10)
     @top_teams = Team.order('amount_raised desc').limit(10)
-    Team.joins(:dancers).group('teams.id').each do |team|
+    Team.joins(:dancers).group('teams.id').each_with_index do |team, index|
       @team = team
       @dancers = @team.dancers.pluck(:email)
       date_range = Date.today.beginning_of_week..Date.today.end_of_week
       @events = Event.where(date_time: date_range).all
-      @ranking = 1
+      @ranking = index + 1
       mail(to: @dancers, subject: "Weekly Update - #{@team.name}")
     end
   end
@@ -18,12 +18,12 @@ class Newsletter < ApplicationMailer
   def test_emails
     @top_dancers = Dancer.joins(:charges).group('dancers.id').order('sum(charges.amount) desc').limit(10)
     @top_teams = Team.order('amount_raised desc').limit(10)
-    Team.joins(:dancers).group('teams.id').limit(5).each do |team|
+    Team.joins(:dancers).group('teams.id').limit(5).each_with_index do |team, index|
       logger.info("SENDING EMAIL TO #{team.name}")
       @team = team
       date_range = Date.today.beginning_of_week..Date.today.end_of_week
       @events = Event.where(date_time: date_range).all
-      @ranking = 1
+      @ranking = index + 1
       mail(to: "laddng12@wfu.edu", subject: "Weekly Update - #{@team.name}", template_name: "weekly_mailer")
     end
   end
