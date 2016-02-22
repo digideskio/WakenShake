c0ce20b2da4505dd5bd6decbf793ef3690f79f99
Nick Ladd <laddng12@wfu.edu>
2016-02-22 18:24:57 -0500

    weekly newsletter for all dancers/teams

--- app/mailers/newsletter.rb
+++ app/mailers/newsletter.rb
@@ -1,13 +1,31 @@
 class Newsletter < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.newsletter.weekly-mailer.subject
-  #
-  def weekly-mailer
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  # weekly email newsletter for teams
+  def weekly_mailer
+    @top_dancers = Dancer.joins(:charges).group('dancers.id').order('sum(charges.amount) desc').limit(10)
+    @top_teams = Team.order('amount_raised desc').limit(10)
+    Team.joins(:dancers).each_with_index do |team, index|
+      @dancers = team.dancers.pluck(:email)
+      @team = team
+      date_range = Date.today.beginning_of_week..Date.today.end_of_week
+      @events = Event.where(date_time: date_range).all
+      @ranking = index + 1
+      mail(to: @dancers, subject: "Weekly Update - #{@team.name}")
+    end
   end
+
+  # test_emails
+  def test_emails
+    @top_dancers = Dancer.joins(:charges).group('dancers.id').order('sum(charges.amount) desc').limit(10)
+    @top_teams = Team.order('amount_raised desc').limit(10)
+    Team.joins(:dancers).limit(5).each_with_index do |team, index|
+      @team = team
+      date_range = Date.today.beginning_of_week..Date.today.end_of_week
+      @events = Event.where(date_time: date_range).all
+      @ranking = index + 1
+      mail(to: "laddng12@wfu.edu", subject: "Weekly Update - #{@team.name}", template_name: "weekly_mailer")
+    end
+  end
+
+
 end
--- lib/tasks/send_emails.rake
+++ lib/tasks/send_emails.rake
@@ -1,6 +1,13 @@
 namespace :send_emails do
   desc "TODO"
-  task weekly-update: :environment do
+  task weekly_update: :environment do
+    if Date.today.wday == 1
+      Newsletter.weekly_mailer.deliver_now
+    end
+  end
+
+  task test_emails: :environment do
+    Newsletter.test_emails.deliver_now
   end
 
 end
