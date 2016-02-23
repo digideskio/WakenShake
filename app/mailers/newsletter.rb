e844c5112ffce23fd974904e6f9a5ef30bbee889
Nick Ladd <laddng12@wfu.edu>
2016-02-22 19:02:37 -0500

    updates to emails

--- app/mailers/newsletter.rb
+++ app/mailers/newsletter.rb
@@ -1,32 +1,13 @@
 class Newsletter < ApplicationMailer
 
   # weekly email newsletter for teams
-  def weekly_mailer
+  def weekly_mailer(team)
     @top_dancers = Dancer.joins(:charges).group('dancers.id').order('sum(charges.amount) desc').limit(10)
     @top_teams = Team.order('amount_raised desc').limit(10)
-    Team.joins(:dancers).group('teams.id').each_with_index do |team, index|
-      @team = team
-      @dancers = @team.dancers.pluck(:email)
-      date_range = Date.today.beginning_of_week..Date.today.end_of_week
-      @events = Event.where(date_time: date_range).all
-      @ranking = index + 1
-      mail(to: @dancers, subject: "Weekly Update - #{@team.name}")
-    end
+    @team = team
+    @dancers = @team.dancers.pluck(:email)
+    date_range = Date.today.beginning_of_week..Date.today.end_of_week
+    @events = Event.where(date_time: date_range).all
+    mail(to: @dancers, subject: "Weekly Update - #{@team.name}")
   end
-
-  # test_emails
-  def test_emails
-    @top_dancers = Dancer.joins(:charges).group('dancers.id').order('sum(charges.amount) desc').limit(10)
-    @top_teams = Team.order('amount_raised desc').limit(10)
-    Team.joins(:dancers).group('teams.id').limit(5).each_with_index do |team, index|
-      logger.info("SENDING EMAIL TO #{team.name}")
-      @team = team
-      date_range = Date.today.beginning_of_week..Date.today.end_of_week
-      @events = Event.where(date_time: date_range).all
-      @ranking = index + 1
-      mail(to: "laddng12@wfu.edu", subject: "Weekly Update - #{@team.name}", template_name: "weekly_mailer")
-    end
-  end
-
-
 end
--- lib/tasks/send_emails.rake
+++ lib/tasks/send_emails.rake
@@ -2,12 +2,9 @@ namespace :send_emails do
   desc "TODO"
   task weekly_update: :environment do
     if Date.today.wday == 1
-      Newsletter.weekly_mailer.deliver_now
+      Team.joins(:dancers).group('teams.id').each do |team|
+        Newsletter.weekly_mailer(team).deliver_now
+      end
     end
   end
-
-  task test_emails: :environment do
-    Newsletter.test_emails.deliver_now
-  end
-
 end
