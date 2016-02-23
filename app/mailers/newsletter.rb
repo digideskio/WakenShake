8bdc1efe833312b4c021338d032bf68ee76bb476
Nick Ladd <laddng12@wfu.edu>
2016-02-22 19:15:27 -0500

    updates

--- app/mailers/newsletter.rb
+++ app/mailers/newsletter.rb
@@ -13,13 +13,14 @@ class Newsletter < ApplicationMailer
 
   # tester for mailer
   def test_weekly_mailer(team)
+    puts "sending emails"
     @top_dancers = Dancer.joins(:charges).group('dancers.id').order('sum(charges.amount) desc').limit(10)
     @top_teams = Team.order('amount_raised desc').limit(10)
     @team = team
     @dancers = @team.dancers.pluck(:email)
     date_range = Date.today.beginning_of_week..Date.today.end_of_week
     @events = Event.where(date_time: date_range).all
-    mail(to: 'laddng12@wfu.edu', subject: "Weekly Update - #{@team.name}")
+    mail(to: 'laddng12@wfu.edu', subject: "Weekly Update - #{@team.name}", template_name: "weekly_mailer")
   end
 
 end
