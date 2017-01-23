d1aca3ef1d1f2ff9432a24cc880cb2d5e66e2c8a
Connor Bulakites <connor.bulakites@gmail.com>
2017-01-23 17:45:56 -0500

    Experiment redirecting to custom payment page

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -29,71 +29,54 @@ class ChargesController < ApplicationController
   # POST /charges
   # POST /charges.json
   def create
-    if not params[:stripeToken].present?
-      charge_record = Charge.create!(charge_params)
-      DonationMailer.donation_notification(charge_record).deliver_later
-      redirect_to(:back)
-    elsif params[:stripeToken].present?
-      # determine if the charge is a donation or registration fee
-      @description = ""
-      if params[:is_donation].present?
-        @description = "Wake \'N Shake Donation"
-      elsif params[:is_registration_fee].present?
-        @description = "Wake \'N Shake Registration Fee"
-      end
 
-      # registration fee amount - $20
-      @amount = 20
+    redirect_to ENV["PAYMENTURL"]
 
-      # if not registration fee, set donation amount
-      if params[:donation_amount].present?
-        @amount = params[:donation_amount].to_i
-      end
+=begin
+    raise RuntimeError "hello"
 
-      customer = Stripe::Customer.create(
-        email: params[:stripeEmail],
-        source: params[:stripeToken]
-      )
-
-      charge = Stripe::Charge.create(
-        customer: customer.id,
-        amount: @amount*100,
-        description: @description,
-        currency: 'usd'
-      )
-
-      # associate the charge to a team or dancer
-      if params[:charge_type] == "Dancer"
-        @dancer = Dancer.find(params[:dancer_id])
-        charge_record = @dancer.charges.new(amount: @amount, email: params[:stripeEmail])
-        if params[:is_donation].present?
-          charge_record.is_donation = true
-          charge_record.save
-        elsif params[:is_registration_fee].present?
-          charge_record.is_registration_fee = true
-          charge_record.save
-        end
-      elsif params[:charge_type] == "Team"
-        @team = Team.find(params[:team_id])
-        charge_record = @team.charges.new(amount: @amount, email: params[:stripeEmail], is_donation: true)
-      elsif params[:charge_type] == "All"
-        charge_record = Charge.new(amount: @amount, email: params[:stripeEmail], is_donation: true)
-      end
-
-      if charge_record.save
-        if charge_record.is_registration_fee.present?
-          redirect_to dancer_path(@dancer)
-        elsif charge_record.is_donation.present?
-          DonationMailer.donation_notification(charge_record).deliver_later
-          redirect_to charge_record
-        end
-      end
-    else
-      redirect_to root_path
+    if params[:is_donation].present?
+      @description = "Wake \'N Shake Donation"
+    elsif params[:is_registration_fee].present?
+      @description = "Wake \'N Shake Registration Fee"
     end
-  rescue Stripe::CardError => e
-    logger.debug e.message
-    redirect_to :back, flash: { error:"#{e.message} Please try again or try another card." }
+
+    # registration fee amount - $20
+    @amount = 20
+
+    # if not registration fee, set donation amount
+    if params[:donation_amount].present?
+      @amount = params[:donation_amount].to_i
+    end
+
+    # associate the charge to a team or dancer
+    if params[:charge_type] == "Dancer"
+      @dancer = Dancer.find(params[:dancer_id])
+      charge_record = @dancer.charges.new(amount: @amount, email: params[:email])
+      if params[:is_donation].present?
+        charge_record.is_donation = true
+        charge_record.save
+      elsif params[:is_registration_fee].present?
+        charge_record.is_registration_fee = true
+        charge_record.save
+      end
+    elsif params[:charge_type] == "Team"
+      @team = Team.find(params[:team_id])
+      charge_record = @team.charges.new(amount: @amount, email: params[:email], is_donation: true)
+    elsif params[:charge_type] == "All"
+      charge_record = Charge.new(amount: @amount, email: params[:email], is_donation: true)
+    end
+
+    if charge_record.save
+      if charge_record.is_registration_fee.present?
+        redirect_to dancer_path(@dancer)
+      elsif charge_record.is_donation.present?
+        DonationMailer.donation_notification(charge_record).deliver_later
+        redirect_to charge_record
+      end
+    end
+=end
+
   end
 
   # PATCH/PUT /charges/1
@@ -136,6 +119,6 @@ class ChargesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def charge_params
-      params.require(:charge).permit(:amount, :charged_id, :charged_type, :is_registration_fee, :is_donation, :stripeEmail, :stripeToken, :email)
+      params.require(:charge).permit(:amount, :charged_id, :charged_type, :is_registration_fee, :is_donation, :email)
     end
 end
