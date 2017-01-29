448150fb8ed905bb34b342082dfb6da67712a79b
Connor Bulakites <connor.bulakites@gmail.com>
2017-01-29 13:23:40 -0500

    Insert missing colon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with :exception
+  protect_from_forgery with: :exception
 
   alias_method :current_user, :current_dancer
 
