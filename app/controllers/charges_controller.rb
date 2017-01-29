2640cbebdf8628e1bb5eb98bf571649d8d8391f1
Connor Bulakites <connor.bulakites@gmail.com>
2017-01-29 12:08:10 -0500

    Update amount_raised incharges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -60,6 +60,8 @@ class ChargesController < ApplicationController
       @team = Team.find(params[:team_id])
       charge_record = @team.charges.new(amount: @amount, email: params[:email], is_donation: true)
       charge_record.save
+      @team.amount_raised += @amount
+      @team.save
     elsif params[:charge_type] == "All"
       charge_record = Charge.new(amount: @amount, email: params[:email], is_donation: true)
       charge_record.save
@@ -73,7 +75,6 @@ class ChargesController < ApplicationController
         if params[:charge_type] == "Dancer"
           redirect_to dancer_path(@dancer), notice: "Your donation was processed successfully!"
         elsif params[:charge_type] == "Team"
-          @team.amount_raised += @amount
           redirect_to team_path(@team), notice: "Your donation was processed successfully!"
         else
           render :action => "show"
