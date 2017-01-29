8a3a4284fdc402b4194bd20c64ea9bfcbf02ee6c
Connor Bulakites <connor.bulakites@gmail.com>
2017-01-29 13:15:01 -0500

    Reactivate site-wide csrf protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery with :exception
 
   alias_method :current_user, :current_dancer
 
