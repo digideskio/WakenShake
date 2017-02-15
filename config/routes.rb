ba31539302ba6178634f0bfdba8c41e9c3708a71
Connor Bulakites <connor.bulakites@gmail.com>
2017-02-15 14:35:23 -0500

    Chang match to get

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :sponsors
   resources :champions
-  match '/past_champions', to: 'champions#past'
+  get '/past_champions', to: 'champions#past'
   resources :events
   get 'committee/index'
   get 'accountant/index'
