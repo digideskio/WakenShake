0788dad45de0dd1a45aef428dee9a2b27b4b8322
Connor Bulakites <connor.bulakites@gmail.com>
2017-02-15 01:46:07 -0500

    Revert "Add AWS Region environment variable"
    This reverts commit fa0969efa5e8b6cf53eeb8920d80067a091996ef.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,6 @@ Rails.application.configure do
   # configuration for paperclip + AWS connection
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_region => ENV['AWS_REGION'],
     :s3_protocol => :https,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
